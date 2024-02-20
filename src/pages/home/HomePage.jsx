d6910611a4b29ba128cf652d3d6b0bf381467710
AsyncXeNo <xeno25502@gmail.com>
2024-02-20 20:44:22 +0530

    ...

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,10 @@ import { useNavigate } from "react-router-dom"
 
 import { logout } from "../../utils"
 
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import Navbar from '../../components/Navbar'
+
 
 const HomePage = () => {
 
@@ -13,10 +17,12 @@ const HomePage = () => {
         navigate('/login')
     }
 
-    return (
-        
+    return (        
         <>
+            <Header />
+            <Navbar />
             <button onClick={logOut}>Log out</button>
+            <Footer />
         </>
     )
     
