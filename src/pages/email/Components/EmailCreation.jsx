d63099dabd33b7af6032e658036bd871d4532707
sagarsharma5 <sagar.sharma.5235@gmail.com>
2024-03-14 14:17:21 +0530

    add email creation

--- src/pages/email/Components/EmailCreation.jsx
+++ src/pages/email/Components/EmailCreation.jsx
@@ -37,7 +37,7 @@
 
 import React from "react";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const EmailCreation = () => {
   const [type, setType] = useState("");
@@ -57,19 +57,54 @@ const EmailCreation = () => {
   const [additional_data, setAdditionalData] = useState("");
   const [payment_url, setPayment_Url] = useState("");
 
+  useEffect(() => {}, [subject]);
+
   const placeholders = {
     1: "Company Name",
     2: "Company Address",
     3: "Company Phone",
   };
 
+  const convertCompoundInputToSentence = (compound) => {
+    let sentence = "";
+    compound.map((part) => {
+      if (typeof part === "string") {
+        sentence += part;
+      } else if (typeof part === "number") {
+        sentence += " " + placeholders[part] + " ";
+      }
+    });
+    return sentence;
+  };
+
   const onChangeSubjectElement = (text, index) => {
-    newSubject = [...subject];
+    const newSubject = [...subject];
     newSubject[index] = text;
     setSubject(newSubject);
   };
+  const onChangeTitleElement = (text, index) => {
+    const newTitle = [...title];
+    newTitle[index] = text;
+    setTitle(newTitle);
+  };
+  const onChangeSubtitleElement = (text, index) => {
+    const newSubtitle = [...subtitle];
+    newSubtitle[index] = text;
+    setSubtitle(newSubtitle);
+  };
+  const onChangePreviewElement = (text, index) => {
+    const newPreview = [...preview];
+    newPreview[index] = text;
+    setPreview(newPreview);
+  };
 
-  const selectInDropdown = (value) => {
+  const onChangeUpperBody = (text, element_index, item_index) => {
+    const newParaBody = [...upper[item_index][1]];
+    newParaBody[element_index] = text;
+    handleBodyChange(item_index, newParaBody);
+  };
+
+  const selectInSubjectDropdown = (value) => {
     if (value === "") {
       setSubject([...subject, value]);
     } else if (value === null) {
@@ -78,9 +113,59 @@ const EmailCreation = () => {
       setSubject([...subject, parseInt(value)]);
     }
   };
+  const selectInTitleDropdown = (value) => {
+    if (value === "") {
+      setTitle([...title, value]);
+    } else if (value === null) {
+      return;
+    } else {
+      setTitle([...title, parseInt(value)]);
+    }
+  };
+  const selectInSubtitleDropdown = (value) => {
+    if (value === "") {
+      setSubtitle([...subtitle, value]);
+    } else if (value === null) {
+      return;
+    } else {
+      setSubtitle([...subtitle, parseInt(value)]);
+    }
+  };
+  const selectInPreviewDropdown = (value) => {
+    if (value === "") {
+      setPreview([...preview, value]);
+    } else if (value === null) {
+      return;
+    } else {
+      setPreview([...preview, parseInt(value)]);
+    }
+  };
+
+  const handleHeadingChange = (index, value) => {
+    const updatedUpperBody = [...upper];
+    updatedUpperBody[index][0] = value;
+    setUpper(updatedUpperBody);
+  };
+
+  const handleBodyChange = (index, value) => {
+    const updatedUpperBody = [...upper];
+    updatedUpperBody[index][1] = value;
+    setUpper(updatedUpperBody);
+  };
+
+  const addHeadingBodyPair = (e) => {
+    e.preventDefault();
+    setUpper([...upper, { heading: "", body: [] }]);
+  };
+
+  const deleteHeadingBodyPair = (e, index) => {
+    e.preventDefault();
+    const updatedUpperBody = upper.filter((_, idx) => idx !== index);
+    setUpper(updatedUpperBody);
+  };
 
   return (
-    <div className="flex justify-center w-full mt-12">
+    <div className="flex justify-center w-full my-12">
       <div className="flex flex-col  items-center w-2/3 bg-nota-cyan h-full bg-nota-blue rounded-lg">
         <span className="font-markazi-text text-4xl my-8 font-bold underline">
           Email Template Creation
@@ -107,21 +192,26 @@ const EmailCreation = () => {
             <span className="font-markazi-text text-2xl font-bold w-[100px]">
               Subject
             </span>
-            <div className="flex items-center  py-4 gap-y-4 gap-x-1 flex-row w-2/3 bg-white rounded-lg flex-wrap justify-evenly px-6">
+            <div className="flex py-3 flex-row w-2/3 gap-y-4 gap-x-1 bg-white rounded-lg flex-wrap px-3">
               {subject.map((element, index) =>
                 typeof element === "string" ? (
                   <input
                     key={index}
                     type="text"
                     placeholder="Text"
-                    className="flex flex-start w-1/4 h-12 items-center bg-gray-300 outline-none text-center rounded"
+                    className={`flex flex-start px-3 h-12 items-center bg-gray-300 outline-none text-center rounded`}
+                    style={{
+                      width: `${
+                        element.length > 15 ? (element.length + 2) * 10 : 170
+                      }px`,
+                    }}
                     value={element}
                     onChange={(e) =>
                       onChangeSubjectElement(e.target.value, index)
                     }
                   />
                 ) : (
-                  <div className="flex flex-start w-1/4 h-12 items-center bg-nota-peach outline-none text-center rounded justify-center items-center text-nota-black">
+                  <div className="flex flex-start flex-1 min-w-36 h-12 items-center bg-nota-peach outline-none text-center rounded justify-center items-center text-nota-black">
                     {placeholders[element]}
                   </div>
                 )
@@ -129,8 +219,8 @@ const EmailCreation = () => {
 
               <select
                 value=""
-                onChange={(e) => selectInDropdown(e.target.value)}
-                className="outline-none w-1/4 bg-gray-300 h-12 rounded pl-1 cursor-pointer"
+                onChange={(e) => selectInSubjectDropdown(e.target.value)}
+                className="outline-none flex-1 min-w-36 bg-gray-300 h-12 rounded pl-1 cursor-pointer"
               >
                 <option disabled value="">
                   Select
@@ -140,6 +230,235 @@ const EmailCreation = () => {
                 <option value={2}>Company Address</option>
                 <option value={3}>Company Phone</option>
               </select>
+            </div>
+          </div>
+
+          <div className="flex flex-row w-full justify-center items-center gap-x-10">
+            <span className="font-markazi-text text-2xl font-bold w-[100px]">
+              Title
+            </span>
+            <div className="flex py-3 flex-row w-2/3 gap-y-4 gap-x-1 bg-white rounded-lg flex-wrap px-3">
+              {title.map((element, index) =>
+                typeof element === "string" ? (
+                  <input
+                    key={index}
+                    type="text"
+                    placeholder="Text"
+                    className={`flex flex-start px-3 h-12 items-center bg-gray-300 outline-none text-center rounded`}
+                    style={{
+                      width: `${
+                        element.length > 15 ? (element.length + 2) * 10 : 170
+                      }px`,
+                    }}
+                    value={element}
+                    onChange={(e) =>
+                      onChangeTitleElement(e.target.value, index)
+                    }
+                  />
+                ) : (
+                  <div className="flex flex-start flex-1 min-w-36 h-12 items-center bg-nota-peach outline-none text-center rounded justify-center items-center text-nota-black">
+                    {placeholders[element]}
+                  </div>
+                )
+              )}
+
+              <select
+                value=""
+                onChange={(e) => selectInTitleDropdown(e.target.value)}
+                className="outline-none flex-1 min-w-36 bg-gray-300 h-12 rounded pl-1 cursor-pointer"
+              >
+                <option disabled value="">
+                  Select
+                </option>
+                <option value={""}>Text</option>
+                <option value={1}>Company Name</option>
+                <option value={2}>Company Address</option>
+                <option value={3}>Company Phone</option>
+              </select>
+            </div>
+          </div>
+          <div className="flex flex-row w-full justify-center items-center gap-x-10">
+            <span className="font-markazi-text text-2xl font-bold w-[100px]">
+              Subtitle
+            </span>
+            <div className="flex py-3 flex-row w-2/3 gap-y-4 gap-x-1 bg-white rounded-lg flex-wrap px-3">
+              {subtitle.map((element, index) =>
+                typeof element === "string" ? (
+                  <input
+                    key={index}
+                    type="text"
+                    placeholder="Text"
+                    className={`flex flex-start px-3 h-12 items-center bg-gray-300 outline-none text-center rounded`}
+                    style={{
+                      width: `${
+                        element.length > 15 ? (element.length + 2) * 10 : 170
+                      }px`,
+                    }}
+                    value={element}
+                    onChange={(e) =>
+                      onChangeSubtitleElement(e.target.value, index)
+                    }
+                  />
+                ) : (
+                  <div className="flex flex-start flex-1 min-w-36 h-12 items-center bg-nota-peach outline-none text-center rounded justify-center items-center text-nota-black">
+                    {placeholders[element]}
+                  </div>
+                )
+              )}
+
+              <select
+                value=""
+                onChange={(e) => selectInSubtitleDropdown(e.target.value)}
+                className="outline-none flex-1 min-w-36 bg-gray-300 h-12 rounded pl-1 cursor-pointer"
+              >
+                <option disabled value="">
+                  Select
+                </option>
+                <option value={""}>Text</option>
+                <option value={1}>Company Name</option>
+                <option value={2}>Company Address</option>
+                <option value={3}>Company Phone</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="flex flex-row w-full justify-center items-center gap-x-10">
+            <span className="font-markazi-text text-2xl font-bold w-[100px]">
+              Preview
+            </span>
+            <div className="flex py-3 flex-row w-2/3 gap-y-4 gap-x-1 bg-white rounded-lg flex-wrap px-3">
+              {preview.map((element, index) =>
+                typeof element === "string" ? (
+                  <input
+                    key={index}
+                    type="text"
+                    placeholder="Text"
+                    className={`flex flex-start px-3 h-12 items-center bg-gray-300 outline-none text-center rounded`}
+                    style={{
+                      width: `${
+                        element.length > 15 ? (element.length + 2) * 10 : 170
+                      }px`,
+                    }}
+                    value={element}
+                    onChange={(e) =>
+                      onChangePreviewElement(e.target.value, index)
+                    }
+                  />
+                ) : (
+                  <div className="flex flex-start flex-1 min-w-36 h-12 items-center bg-nota-peach outline-none text-center rounded justify-center items-center text-nota-black">
+                    {placeholders[element]}
+                  </div>
+                )
+              )}
+
+              <select
+                value=""
+                onChange={(e) => selectInPreviewDropdown(e.target.value)}
+                className="outline-none flex-1 min-w-36 bg-gray-300 h-12 rounded pl-1 cursor-pointer"
+              >
+                <option disabled value="">
+                  Select
+                </option>
+                <option value={""}>Text</option>
+                <option value={1}>Company Name</option>
+                <option value={2}>Company Address</option>
+                <option value={3}>Company Phone</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="flex flex-row w-full justify-center items-center gap-x-10">
+            <span className="font-markazi-text text-2xl font-bold w-[100px]">
+              Above Email Body
+            </span>
+            <input
+              type="text"
+              value={type}
+              onChange={(e) => setAboveEmailBody(e.target.value)}
+              placeholder="Enter Text"
+              className="flex justify-center items-center rounded-lg w-2/3 h-16 text-center outline-none"
+            />
+          </div>
+
+          <div className="w-full flex flex-col justify-center items-center  mt-12">
+            <div className="flex flex-col w-full justify-center items-center gap-y-10">
+              <span className="font-markazi-text text-3xl font-bold underline">
+                Upper Body
+              </span>
+              <button
+                className="rounded-full font-markazi-text text-xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+                onClick={addHeadingBodyPair}
+              >
+                Add Heading & Body
+              </button>
+              {upper.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col w-full justify-center items-center gap-y-5 transition-opacity duration-500 ease-in-out"
+                  style={{ opacity: 1 }}
+                >
+                  <input
+                    type="text"
+                    value={item[0]}
+                    onChange={(e) => handleHeadingChange(index, e.target.value)}
+                    placeholder="Heading"
+                    className="flex justify-center items-center rounded-lg w-2/3 h-16 text-center outline-none"
+                  />
+
+                  <div className="flex py-3 flex-row w-2/3 gap-y-4 gap-x-1 bg-white rounded-lg flex-wrap px-3">
+                    {item[1].map((element, elementIndex) =>
+                      typeof element === "string" ? (
+                        <input
+                          key={elementIndex}
+                          type="text"
+                          placeholder="Text"
+                          className={`flex flex-start px-3 h-12 items-center bg-gray-300 outline-none text-center rounded`}
+                          style={{
+                            width: `${
+                              element.length > 15
+                                ? (element.length + 2) * 10
+                                : 170
+                            }px`,
+                          }}
+                          value={element}
+                          onChange={(e) =>
+                            onChangeUpperBody(
+                              e.target.value,
+                              elementIndex,
+                              index
+                            )
+                          }
+                        />
+                      ) : (
+                        <div className="flex flex-start flex-1 min-w-36 h-12 items-center bg-nota-peach outline-none text-center rounded justify-center items-center text-nota-black">
+                          {placeholders[element]}
+                        </div>
+                      )
+                    )}
+                    {/* TODO */}
+                    <select
+                      value=""
+                      onChange={(e) => selectInPreviewDropdown(e.target.value)}
+                      className="outline-none flex-1 min-w-36 bg-gray-300 h-12 rounded pl-1 cursor-pointer"
+                    >
+                      <option disabled value="">
+                        Select
+                      </option>
+                      <option value={""}>Text</option>
+                      <option value={1}>Company Name</option>
+                      <option value={2}>Company Address</option>
+                      <option value={3}>Company Phone</option>
+                    </select>
+                  </div>
+
+                  <button
+                    className="rounded-full font-markazi-text text-xl bg-red-500 hover:bg-red-700 text-white font-bold py-2  px-3 "
+                    onClick={(e) => deleteHeadingBodyPair(e, index)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
         </form>
